150b86ced59ded25148bef2a502eea9deb54c788
quang <phanhaiquang@gmail.com>
2016-06-13 07:03:59 +0700

    use default order in model, not views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  default_scope { order(:id) }
+
   has_many :predictions
 
   def name
