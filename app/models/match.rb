c7390f0c98fd31b3e46f902d1f1b0f82e7483390
buithanhcong <thanhcong.xyz@gmail.com>
2018-07-01 19:06:49 +0700

    fix save reward set to 0 before take to calculate

--- app/models/match.rb
+++ app/models/match.rb
@@ -149,11 +149,11 @@ class Match < ActiveRecord::Base
       if prediction_winners_count == 0
         cup.update_attributes(save_reward: cup.knockout_match_fee*predictions.count)
       else
-        cup.update_attributes(save_reward: 0)
         predictions.all.each do |prediction|
           next if !prediction.win?
           prediction.update_attributes(reward: ((cup.knockout_match_fee*predictions.count+cup.save_reward)/prediction_winners_count).round)
         end
+        cup.update_attributes(save_reward: 0)
       end
     else
       predictions.all.each do |prediction|
