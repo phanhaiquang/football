b86d4e6086ea0c36cc007cb7992653221f926111
buithanhcong <thanhcong.xyz@gmail.com>
2018-07-02 19:59:32 +0700

    update knockout prediction method

--- app/models/match.rb
+++ app/models/match.rb
@@ -31,9 +31,9 @@ class Match < ActiveRecord::Base
   def fullscores
     if !subscore1.nil? && !subscore2.nil?
       if !penscore1.nil? && !penscore2.nil?
-        "#{mainscore1+subscore1} - #{mainscore2+subscore2}" + " (#{penscore1} - #{penscore2})"
+        subscores + " (#{penscore1} - #{penscore2})"
       else
-        "#{mainscore1+subscore1} - #{mainscore2+subscore2}"
+        subscores
       end
     else
       mainscores
